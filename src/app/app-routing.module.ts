b2dce1c9f855d64e36b38c265f41c9d784a68398
Antoine Varnerot <avarnerot@yahoo.fr>
2022-12-01 15:40:43 +0100

    gg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppComponent,
-    children: [
-      {
-        path: '',
-        component: HomeComponent
-      },
-      {
-        path: 'orga',
-        loadChildren: () => import('./organizer-dashboard/organizer-dashboard.module').then(mod => mod.OrganizerDashboardModule)
-      },
-      {
-        path: 'public',
-        loadChildren: () => import('./public/public.module').then(mod => mod.PublicModule)
-      },
-    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
