2cf430ee63b4537c171110698f4b2f454e51495c
Antoine Varnerot <avarnerot@yahoo.fr>
2022-12-23 18:57:07 +0100

    add review in company class

--- src/app/models/company.model.ts
+++ src/app/models/company.model.ts
@@ -1,15 +1,23 @@
 import { User } from "./user.model";
 
 export interface Company {
-  id: string;
+  id: number;
   name: string;
   address: string;
   employees?: User[];
   isDeleted: boolean;
   pictures?: string[];
   rating?: number;
+  reviews: Review[];
 }
 
+export interface Review {
+    id: number;
+    company: Company;
+    date: number;
+    content: string;
+    rating: number;
+}
 export interface Address {
   libelle: string;
   city: string;
--- src/app/public/public-home/public-home.component.ts
+++ src/app/public/public-home/public-home.component.ts
@@ -13,11 +13,27 @@ export class PublicHomeComponent implements OnInit {
   
   ngOnInit(): void {
     this.company = {
-        id: "1",
+        id: 1,
         name: "Dream Five",
         address: "5 rue du foot, Marseille",
         isDeleted: false,
         rating: 3,
+        reviews: [
+            {
+                id: 1,
+                company: this.company,
+                date: Date.now(),
+                content: "2 terrains fonctionnels..",
+                rating: 3,
+            },
+            {
+                id: 2,
+                company: this.company,
+                date: Date.now(),
+                content: "Bof...",
+                rating: 2,
+            }
+        ]
     }
   }
 
