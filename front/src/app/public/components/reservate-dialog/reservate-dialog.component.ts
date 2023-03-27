b6c2ce98ac8dcd32da7e4fb61ffafc0ca59a201d
Antoine Varnerot <avarnerot@yahoo.fr>
2023-03-27 19:25:46 +0200

    add new service and finish table

--- front/src/app/public/components/reservate-dialog/reservate-dialog.component.ts
+++ front/src/app/public/components/reservate-dialog/reservate-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ReservationService } from 'src/app/services/reservation.service';
 
 @Component({
   selector: 'app-reservate-dialog',
@@ -9,13 +10,15 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class ReservateDialogComponent implements OnInit {
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public company: any
+    @Inject(MAT_DIALOG_DATA) public company: any,
+    private reservationService: ReservationService,
   ) {}
 
   currentDay: Date = new Date();
   currentDayString = this.currentDay.toDateString();
   dayBefore: string = this.currentDay.setDate(this.currentDay.getDate() - 1).toString();
   dayAfter: string = this.currentDay.setDate(this.currentDay.getDate() + 2).toString();
+  reservatedHour: string = '';
 
   displayedColumns: string[] = ['col1','col2', 'col3', 'col4'];
   dataSource: any=[];
@@ -23,11 +26,21 @@ export class ReservateDialogComponent implements OnInit {
   ngOnInit(): void {
     const ELEMENT_DATA: {}[] = [
       {col1: '8', col2: '9', col3: '10', col4: '11'},
-      {col1: '12', col2: '13', col3: '', col4: ''},
-      {col1: '10', col2: '', col3: '', col4: ''},
-      {col1: '11', col2: '', col3: '', col4: ''},
+      {col1: '12', col2: '13', col3: '14', col4: '15'},
+      {col1: '16', col2: '17', col3: '18', col4: '19'},
+      {col1: '20', col2: '21', col3: '22', col4: '23'},
     ];
     this.dataSource = ELEMENT_DATA;
   }
 
+  selectHour(val: string){
+    this.reservatedHour = val;
+    console.log(val)
+  }
+
+  reservate(){
+    if(this.reservatedHour){
+      this.reservationService.addNewReservation(this.reservatedHour, this.company.id);
+    }
+  }
 }
---
+++ front/src/app/services/reservation.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  constructor() { }
+
+  addNewReservation(hour: string, idCompany: number | string){
+
+  }
+}
